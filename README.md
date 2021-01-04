c6ccf7d326851766c6a3e4163edbf9cb6cfa0b7f
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-01-04 13:01:04 -0300

    Updates Readme

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 👋 Olá, Seja Bem-vindo(a) ao controle remoto de carrinho ou tratores
 
-# Tecnologias
+# Tecnologias envolvidas
 
-- App Inventor 2
+- Arduino
 - Arduino Bluetooth
+- Android
+- App Inventor 2
+- Bluetooth HC-06
+
+# Objetivo
+
+Criar um controle remoto que envia sinais bluetooth através de um APK Android para um receptor bluetooth associado a uma placa Arduino Mega 2560 R3 dessa forma tendo podendo criar um carrinho de controle remoto ou um trator de controle remoto com rodas.
 
 # Referências utilizadas
 
 [1° Bluetooth HC-06](https://www.filipeflop.com/blog/tutorial-arduino-bluetooth-hc-05-mestre/)  
 [2° Arduino Mega 2560 r3](https://www.filipeflop.com/produto/placa-mega-2560-r3-cabo-usb-para-arduino/)  
+[3° MIT APP Inventor 2](https://appinventor.mit.edu/explore/ai2)  
--- car_or_tractor/car_or_tractor.ino
+++ car_or_tractor/car_or_tractor.ino
@@ -1,4 +1,4 @@
-//Developed by Claudi Martins de Freitas // Desenvolvido por Claudi Martins de Freitas 
+//Developed by Claudi Martins de Freitas // Desenvolvido por Claudi Martins de Freitas
 //It's a open source project, enjoy it! // É um projeto Open Source, divirta-se
 //The comments are in portuguese, sorry about that
 //This is a project to remote control car or remote control tractor with wheels //  é um projeto para carrinho de controle remoto ou trator de controle remoto com rodas
@@ -10,23 +10,21 @@
 
 char buffer[BUFFER];  // VARIÁVEL DE ARMAZENAMENTO DOS CARACTERES
 int sofar;  // TREM DE PULSO
-int leitura_zero=0;//VERIFICAR A LEITURA DA SERIAL 1
-int disparo=150;//TEMPO DE DISPARO DIREÇÃO
-int disparo2=50;//TEMPO DE DISPARO CARREGADEIRA
-int disparo3=150;//TEMPO DE DISPARO CONJUNTO SUBIDA
-int disparo4=30;//TEMPO DE DISPARO CONJUNTO DESCIDA
+int leitura_zero=0; //VERIFICAR A LEITURA DA SERIAL 1
+int disparo=150;  //TEMPO DE DISPARO DIREÇÃO
+int disparo2=50;  //TEMPO DE DISPARO CARREGADEIRA
+int disparo3=150; //TEMPO DE DISPARO CONJUNTO SUBIDA
+int disparo4=30;  //TEMPO DE DISPARO CONJUNTO DESCIDA
 
 //NUMERAÇÃO DAS PORTAS
-const int frente=2;// CONSTANTE FRENTE É O PINO DIGITAL 2
-const int tras=3;// CONSTANTE TRÁS É O PINO DIGITAL 3
-const int esquerda=4;// CONSTANTE ESQUERDA É O PINO DIGITAL 4
-const int direita=5;// CONSTANTE DIREITA É O PINO DIGITAL 5
-const int sobe_carregadeira=6;// CONSTANTE SOBE CARREGADEIRA É O PINO DIGITAL 6
-const int desce_carregadeira=7;// CONSTANTE DESCE CARREGADEIRA É O PINO DIGITAL 7
-const int sobe_conjunto=8;// CONSTANTE SOBE CONJUNTO É O PINO DIGITAL 8
-const int desce_conjunto=9;// CONSTANTE DESCE CONJUNTO É O PINO DIGITAL 9
-//int sensorMagnetico = A0;// PINO DO SENSOR MAGNÉTICO É A0
-//sensorValue = analogRead(sensorPin);
+const int frente=2; // CONSTANTE FRENTE É O PINO DIGITAL 2
+const int tras=3; // CONSTANTE TRÁS É O PINO DIGITAL 3
+const int esquerda=4; // CONSTANTE ESQUERDA É O PINO DIGITAL 4
+const int direita=5;  // CONSTANTE DIREITA É O PINO DIGITAL 5
+const int sobe_carregadeira=6;  // CONSTANTE SOBE CARREGADEIRA É O PINO DIGITAL 6
+const int desce_carregadeira=7; // CONSTANTE DESCE CARREGADEIRA É O PINO DIGITAL 7
+const int sobe_conjunto=8;  // CONSTANTE SOBE CONJUNTO É O PINO DIGITAL 8
+const int desce_conjunto=9; // CONSTANTE DESCE CONJUNTO É O PINO DIGITAL 9
 
 //ROTINA
 float parsenumber(char code,float val) {//ROTINA PARA DESMEMBRAR CÓGIDO DA SERIAL
@@ -38,23 +36,16 @@ float parsenumber(char code,float val) {//ROTINA PARA DESMEMBRAR CÓGIDO DA SERI
     ptr=strchr(ptr,' ')+1;
   }
   return val;
-} 
-
-//ESCREVER O TEXTO DA SERIAL E DIVIDE EM TIPO STRING OU FLOAT
-void output(char *code,float val) {
-  //Serial.print(code);
-  //Serial.println(val);  
 }
 
-//EXIBIR O NOME DO GRUPO NA SERIAL
+//EXIBIR MENSAGEM NA SERIAL
 void help() {
-  //Serial.println(F("Grupo "));
+  Serial.println(F("Mensagem "));
 }
 
 //PREPARA A ENTRADA PARA RECEBER AS MENSAGENS DA SERIAL
 void ready() {
   sofar=0;  // LIMPAR A ENTRADA DO BUFFER
-  //Serial.print(F(">"));  // RECEBER SINAL NA ENTRADA
 }
 
 //ETAPA DE SETUP, FAZ QUANDO INICIA
@@ -65,27 +56,25 @@ void setup() {
   ready();
 
   //PREPARAR ENTRADAS OU SAÍDAS
-  pinMode(frente, OUTPUT);//FRENTE
-  pinMode(tras, OUTPUT);//TRÁS
-  pinMode(esquerda, OUTPUT);//ESQUERDA
-  pinMode(direita, OUTPUT);//DIREITA
-  pinMode(sobe_carregadeira, OUTPUT);//SOBE CARREGADEIRA
-  pinMode(desce_carregadeira, OUTPUT);//DESCE CARREGADEIRA
-  pinMode(sobe_conjunto, OUTPUT);//SOBE CONJUNTO
-  pinMode(desce_conjunto, OUTPUT);//DESCE CONJUNTO
-  //pinMode(sensorMagnetico, INPUT);//SENSOR MAGNÉTICO
-  //digitalWrite(5, HIGH);//Desliga 
+  pinMode(frente, OUTPUT);  //FRENTE
+  pinMode(tras, OUTPUT);  //TRÁS
+  pinMode(esquerda, OUTPUT);  //ESQUERDA
+  pinMode(direita, OUTPUT); //DIREITA
+  pinMode(sobe_carregadeira, OUTPUT); //SOBE CARREGADEIRA
+  pinMode(desce_carregadeira, OUTPUT);  //DESCE CARREGADEIRA
+  pinMode(sobe_conjunto, OUTPUT); //SOBE CONJUNTO
+  pinMode(desce_conjunto, OUTPUT);  //DESCE CONJUNTO
 }
 
 //LAÇO DE REPETIÇÃO
 void loop() {
   // LISTAGEM DE COMANDOS DA SERIAL
-  
+
   if(Serial.available() > 0) {
-  
+
   while(Serial.available() > 0) {  //HAVENDO ALGO A RECEBER
     char c=Serial.read();  //PEGAR VALOR RECEBIDO
-    //Serial.print(c);  //IMPRIMI A MENSAGEM RECEBIDA
+    Serial.print(c);  //IMPRIMIR A MENSAGEM RECEBIDA
     if(sofar<BUFFER) buffer[sofar++]=c;  // FECHA PACOTE DE MENSAGEM
     if(buffer[sofar-1]==';') break;  //MENSAGEM COMPLETA
   }
@@ -105,12 +94,12 @@ void loop() {
   }
   if(leitura_zero >= 3) {
   modulo(0);
-  }  
+  }
 }
 
 //Leia os comandos de input do Buffer e reconhece qualquer dos comandos separando os por linha.
 void painel_de_comando() {
-  
+
   int cmd = parsenumber('J',-1);
 
   switch(cmd) {
@@ -164,7 +153,7 @@ void painel_de_comando() {
     break;
     }
   }
-  
+
   cmd = parsenumber('C',-1);
 
   switch(cmd) {
@@ -193,7 +182,7 @@ void painel_de_comando() {
     break;
     };
   }
-  
+
 }
 
 //MÓDULO ACIONADO DENTRO DO LOOP RESPONSÁVEL PELOS ACIONAMENTOS
@@ -203,96 +192,95 @@ void modulo(int x)
     if(x == 0)
      {
         //DESLIGA TODAS AS PORTAS POR SEGURANÇA QUANDO NÃO HOUVER COMANDO
-        digitalWrite(frente, HIGH);//DESLIGA FRENTE
-        digitalWrite(tras, HIGH);//DESLIGA TRÁS
-        digitalWrite(esquerda, HIGH);//DESLIGA ESQUERDA
-        digitalWrite(direita, HIGH);//DESLIGA DIREITA  
-        digitalWrite(sobe_carregadeira, HIGH);//DESLIGA SOBE_CARREGADEIRA
-        digitalWrite(desce_carregadeira, HIGH);//DESLIGA DESCE_CARREGADEIRA
-        digitalWrite(sobe_conjunto, HIGH);//DESLIGA SOBE_CONJUNTO
-        digitalWrite(desce_conjunto, HIGH);//DESLIGA DESCE_CONJUNTO
+        digitalWrite(frente, HIGH); //DESLIGA FRENTE
+        digitalWrite(tras, HIGH); //DESLIGA TRÁS
+        digitalWrite(esquerda, HIGH); //DESLIGA ESQUERDA
+        digitalWrite(direita, HIGH);  //DESLIGA DIREITA
+        digitalWrite(sobe_carregadeira, HIGH);  //DESLIGA SOBE_CARREGADEIRA
+        digitalWrite(desce_carregadeira, HIGH); //DESLIGA DESCE_CARREGADEIRA
+        digitalWrite(sobe_conjunto, HIGH);  //DESLIGA SOBE_CONJUNTO
+        digitalWrite(desce_conjunto, HIGH); //DESLIGA DESCE_CONJUNTO
      }
   //1 = FRENTE
   else if(x == 1)
   {
-        digitalWrite(tras, HIGH);//DESLIGA TRÁS  
-        digitalWrite(frente, LOW);//LIGA FRENTE        
+        digitalWrite(tras, HIGH); //DESLIGA TRÁS
+        digitalWrite(frente, LOW);  //LIGA FRENTE
   }
   //2 = TRÁS
   else if(x == 2)
   {
-        digitalWrite(frente, HIGH);//DESLIGA FRENTE
-        digitalWrite(tras, LOW);//LIGA TRÁS               
+        digitalWrite(frente, HIGH); //DESLIGA FRENTE
+        digitalWrite(tras, LOW);  //LIGA TRÁS
   }
   //3 = ESQUERDA
   else if(x == 3)
   {
-        digitalWrite(direita, HIGH);//DESLIGA DIREITA    
-        digitalWrite(esquerda, LOW);//LIGA ESQUERDA       
+        digitalWrite(direita, HIGH);  //DESLIGA DIREITA
+        digitalWrite(esquerda, LOW);  //LIGA ESQUERDA
   }
   //4 = DIREITA
   else if(x == 4)
   {
-        digitalWrite(esquerda, HIGH);//DESLIGA ESQUERDA
-        digitalWrite(direita, LOW);//LIGA DIREITA          
+        digitalWrite(esquerda, HIGH); //DESLIGA ESQUERDA
+        digitalWrite(direita, LOW); //LIGA DIREITA
   }
   //5 = FRENTE + DIREITA
   else if(x == 5)
   {
-        digitalWrite(tras, HIGH);//DESLIGA TRÁS
-        digitalWrite(esquerda, HIGH);//DESLIGA ESQUERDA
-        digitalWrite(frente, LOW);//LIGA FRENTE
-        digitalWrite(direita, LOW);//LIGA DIREITA        
+        digitalWrite(tras, HIGH); //DESLIGA TRÁS
+        digitalWrite(esquerda, HIGH); //DESLIGA ESQUERDA
+        digitalWrite(frente, LOW);  //LIGA FRENTE
+        digitalWrite(direita, LOW); //LIGA DIREITA
   }
   //6 = FRENTE + ESQUERDA
   else if(x == 6)
   {
-        digitalWrite(frente, HIGH);//DESLIGA FRENTE
-        digitalWrite(direita, HIGH);//DESLIGA DIREITA
-        digitalWrite(frente, LOW);//LIGA FRENTE
-        digitalWrite(esquerda, LOW);//LIGA ESQUERDA        
+        digitalWrite(frente, HIGH); //DESLIGA FRENTE
+        digitalWrite(direita, HIGH);  //DESLIGA DIREITA
+        digitalWrite(frente, LOW);  //LIGA FRENTE
+        digitalWrite(esquerda, LOW);  //LIGA ESQUERDA
   }
   //7 = TRÁS + DIREITA
   else if(x == 7)
   {
-        digitalWrite(frente, HIGH);//DESLIGA FRENTE
-        digitalWrite(esquerda, HIGH);//LIGA ESQUERDA 
-        digitalWrite(tras, LOW);//LIGA TRÁS
-        digitalWrite(direita, LOW);//DESLIGA DIREITA     
+        digitalWrite(frente, HIGH); //DESLIGA FRENTE
+        digitalWrite(esquerda, HIGH); //LIGA ESQUERDA
+        digitalWrite(tras, LOW);  //LIGA TRÁS
+        digitalWrite(direita, LOW); //DESLIGA DIREITA
   }
   //8 = TRÁS + ESQUERDA
   else if(x == 8)
   {
-        digitalWrite(frente, HIGH);//DESLIGA FRENTE
-        digitalWrite(direita, HIGH);//DESLIGA DIREITA
-        digitalWrite(tras, LOW);//LIGA TRÁS
-        digitalWrite(esquerda, LOW);//LIGA ESQUERDA     
+        digitalWrite(frente, HIGH); //DESLIGA FRENTE
+        digitalWrite(direita, HIGH);  //DESLIGA DIREITA
+        digitalWrite(tras, LOW);  //LIGA TRÁS
+        digitalWrite(esquerda, LOW);  //LIGA ESQUERDA
   }
   //9 = SOBE CARREGADEIRA
   else if(x == 9)
   {
-        digitalWrite(desce_carregadeira, HIGH);//DESLIGA DESCE_CARREGADEIRA
-        digitalWrite(sobe_carregadeira, LOW);//LIGA SOBE_CARREGADEIRA     
+        digitalWrite(desce_carregadeira, HIGH); //DESLIGA DESCE_CARREGADEIRA
+        digitalWrite(sobe_carregadeira, LOW); //LIGA SOBE_CARREGADEIRA
   }
   //10 = DESCE CARREGADEIRA
   else if(x == 10)
   {
-        digitalWrite(sobe_carregadeira, HIGH);//DESLIGA SOBE_CARREGADEIRA 
-        digitalWrite(desce_carregadeira, LOW);//LIGA DESCE_CARREGADEIRA   
+        digitalWrite(sobe_carregadeira, HIGH);  //DESLIGA SOBE_CARREGADEIRA
+        digitalWrite(desce_carregadeira, LOW);  //LIGA DESCE_CARREGADEIRA
   }
   //11 = SOBE CONJUNTO
   else if(x == 11)
   {
-        digitalWrite(desce_conjunto, HIGH);//DESLIGA DESCE_CONJUNTO 
-        digitalWrite(sobe_conjunto, LOW);//LIGA SOBE_CONJUNTO 
+        digitalWrite(desce_conjunto, HIGH); //DESLIGA DESCE_CONJUNTO
+        digitalWrite(sobe_conjunto, LOW); //LIGA SOBE_CONJUNTO
   }
   //12 = DESCE CONJUNTO
   else if(x == 12)
-  { 
-        digitalWrite(sobe_conjunto, HIGH);//LIGA SOBE_CONJUNTO
-        digitalWrite(desce_conjunto, LOW);//DESLIGA DESCE_CONJUNTO 
+  {
+        digitalWrite(sobe_conjunto, HIGH);  //LIGA SOBE_CONJUNTO
+        digitalWrite(desce_conjunto, LOW);  //DESLIGA DESCE_CONJUNTO
   }
-   //imprime o caracter enviado na serial (Para teste)
-   //Serial.println(x);
+   imprime o caracter enviado na serial (Para teste)
+   Serial.println(x);
 }
-
